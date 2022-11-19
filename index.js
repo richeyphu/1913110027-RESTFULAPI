081f568bbeb33a0ee2f1d6b6efaeea21c7874e22
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-11-19 11:21:42 +0700

    Create .gitigore

---
+++ index.js
@@ -0,0 +1,44 @@
+// const datefns = require('date-fns');
+
+// console.log(datefns.format(new Date(), "MM/dd/yyyy"))
+
+// console.log("Hello World!")
+
+// let firstName = "Phurit";
+// let age = 22;
+
+// console.log(firstName + age)
+
+// let info = `
+//     My name is ${firstName}
+//     My age is ${age}
+// `
+
+// console.log(info)
+
+const user = {
+  name: "Phurit",
+  salary: 1_000_000,
+  address: {
+    province: "Bangkok",
+    postcode: 10250,
+  },
+};
+
+// console.log(user);
+// console.log(user.address.postcode);
+
+// const showData = () => `${user.name}`;
+// console.log(showData());
+
+// const showData2 = () => {
+//   let info = "My name is";
+//   return `${info} ${user.name}`;
+// };
+// console.log(showData2());
+
+// const sumNumber = (a, b) => a + b;
+// console.log(sumNumber(9, 1));
+
+// const { name, salary } = user;
+// console.log(name);
