6b62028d9839fec16be6542b66635647e9bde711
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-11-19 12:05:46 +0700

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,15 @@
+const drinks = ["Coffee", "Coke", "Green Tea"];
+
+for (const d of drinks) {
+  console.log(d);
+}
+
+const users = [
+  { name: "A", age: 16 },
+  { name: "B", age: 18 },
+  { name: "C", age: 20 },
+];
+
+for (const u of users) {
+  console.log(`${u.name} is ${u.age} years old`);
+}
